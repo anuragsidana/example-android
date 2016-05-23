d79eb705f13960dff4eae315b306144c22b4d059
Ulhas Mandrawadkar <ulhas.sm@gmail.com>
2016-05-23 14:53:17 +0530

    Fixes compiler error

--- driver/src/main/java/io/hypertrack/driver/TripActivity.java
+++ driver/src/main/java/io/hypertrack/driver/TripActivity.java
@@ -1,13 +1,10 @@
 package io.hypertrack.driver;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
-import io.hypertrack.example.R;
+import io.hypertrack.driver.R;
 
 public class TripActivity extends AppCompatActivity {
 
@@ -17,15 +14,6 @@ public class TripActivity extends AppCompatActivity {
         setContentView(R.layout.activity_trip);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
