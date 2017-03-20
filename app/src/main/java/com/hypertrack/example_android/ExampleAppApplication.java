3b3f2bb12a7fad3a79f040b0fe40d5d753cc91d3
Piyush Gupta <piyush@hypertrack.io>
2017-03-21 02:09:38 +0530

    Updates example app to HyperTrack v3 Android SDK

---
+++ app/src/main/java/com/hypertrack/example_android/ExampleAppApplication.java
@@ -0,0 +1,21 @@
+package com.hypertrack.example_android;
+
+import android.app.Application;
+
+import com.hypertrack.lib.HyperTrack;
+
+/**
+ * Created by piyush on 23/09/16.
+ */
+public class ExampleAppApplication extends Application{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Initialize HyperTrack SDK with the Publishable Key
+        // Refer to documentation at https://docs.hypertrack.com/v3/gettingstarted/authentication.html#publishable-key
+        // @NOTE: Add **YOUR_PUBLISHABLE_KEY** here for SDK to be authenticated with HyperTrack Server
+        HyperTrack.initialize(this, "pk_4d4fb6fd2e302602581a7bcebb62e8c4bce268d8");
+    }
+}
