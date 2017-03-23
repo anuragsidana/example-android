c4bda49c01b5bdceb63d2030801c89a132b6d09a
Piyush Gupta <piyush@hypertrack.io>
2017-03-23 14:12:32 +0530

    Updates Live tracking view customization code snippets

--- app/src/main/java/com/hypertrack/example_android/UserMapActivity.java
+++ app/src/main/java/com/hypertrack/example_android/UserMapActivity.java
@@ -1,12 +1,16 @@
 package com.hypertrack.example_android;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.hypertrack.lib.HyperTrack;
-import com.hypertrack.lib.internal.consumer.view.HyperTrackMapFragment;
+import com.hypertrack.lib.HyperTrackMapAdapter;
+import com.hypertrack.lib.HyperTrackMapFragment;
+import com.hypertrack.lib.MapFragmentCallback;
 
 import java.util.ArrayList;
 
@@ -30,7 +34,37 @@ public class UserMapActivity extends AppCompatActivity {
         // Initialize Map Fragment added in Activity Layout to getMapAsync
         // Once map is created onMapReady callback will be fire with GoogleMap object
         HyperTrackMapFragment htMapFragment = (HyperTrackMapFragment) getSupportFragmentManager().findFragmentById(R.id.htMapfragment);
+
+        /**
+         * Call the method below to enable UI customizations for Live Tracking View, an instance of HyperTrackMapAdapter needs to be set as depicted below
+         */
+         htMapFragment.setHTMapAdapter(new MyMapAdapter(this));
+
+        /*
+         * Call the method below to register for any callbacks/updates on Live Tracking View/Map
+         */
+        htMapFragment.setMapFragmentCallback(mapFragmentCallback);
     }
+
+    public class MyMapAdapter extends HyperTrackMapAdapter {
+        public MyMapAdapter(Context mContext) {
+            super(mContext);
+        }
+
+        @Override
+        public boolean showHeroMarkerForActionID(HyperTrackMapFragment hyperTrackMapFragment, String actionID) {
+            // check if my action to be tracked is pickup action or dropoff action
+            // and disable hero marker for pickup action
+            return super.showHeroMarkerForActionID(hyperTrackMapFragment, actionID);
+        }
+    }
+
+    private MapFragmentCallback mapFragmentCallback = new MapFragmentCallback() {
+        @Override
+        public void onMapReadyCallback(HyperTrackMapFragment hyperTrackMapFragment, GoogleMap map) {
+            // Handle onMapReady callback here
+        }
+    };
 
     @Override
     public void onBackPressed() {
